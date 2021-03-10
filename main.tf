ded5d414180d8087253e5f0cb669d8a64aa1b111
Vadim Rutkovsky <vrutkovs@redhat.com>
2021-03-10 12:30:42 +0100

    Remove creating SRV records - these are no longer required

--- main.tf
+++ main.tf
@@ -72,8 +72,6 @@ module "dns" {
   base_domain              = var.base_domain
   cluster_domain           = "${var.clustername}.${var.base_domain}"
   cluster_id               = var.cluster_id
-  etcd_count               = length(var.aws_azs)
-  etcd_ip_addresses        = flatten(module.masters.ip_addresses)
   tags                     = local.tags
   vpc_id                   = module.vpc.vpc_id
   publish_strategy         = var.aws_publish_strategy
